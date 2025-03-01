2f002407def2b057f8a5a7598e6c9115822d02fa
Ivan Kalugin <42765419+iklove2@users.noreply.github.com>
2025-03-01 18:10:46 +0300

    add another product sbc.sovacoffeeshop.ru

---
+++ sbc.sovacoffeeshop.ru/js/data.js
@@ -0,0 +1,35 @@
+const dataJson = `[
+  { 
+    "ahrefbye" : "http://tsa.sovacoffeeshop.ru",
+    "ahrefdetails" : "http://tsa.sovacoffeeshop.ru",
+    "img" : "img/tasamayaamerica.webp",
+    "title" : "Эспрессо",
+    "content" : "Арабика и Робуста кофе - темный ночь, сладкий как клюбовь",
+    "weight200" : 200,
+    "price200" : 811,
+    "weight1000" : 1000,
+    "price1000" : 3035
+  },
+  { 
+    "ahrefbye" : "http://abc.sovacoffeeshop.ru",
+    "ahrefdetails" : "http://abc.sovacoffeeshop.ru",
+    "img" : "img/arabikanokofein.jpg",
+    "title" : "Эспрессо",
+    "content" : "Арабика без кофеина вкусный кофе и без кофеина",
+    "weight200" : 200,
+    "price200" : 811,
+    "weight1000" : 1000,
+    "price1000" : 3035
+  },
+  { 
+    "ahrefbye" : "#",
+    "ahrefdetails" : "#",
+    "img" : "img/keniyamurunuru.jpg",
+    "title" : "Эспрессо",
+    "content" : "Кения Муруее Нгуруери с характером ягод, томатиков и улуна",
+    "weight200" : 200,
+    "price200" : 854,
+    "weight1000" : 1000,
+    "price1000" : 3247
+  }
+]`
